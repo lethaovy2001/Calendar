307d86498cbf904aa75951df0a7d69fc64ff97c7
Vy Le <lethaovy2001@gmail.com>
2020-06-10 07:42:48 -0500

    navigate to other screens from dailyTasksScreen

---
+++ Calendar/UserSettingsScreen/UserSettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UserSettingsViewController.swift
+//  Calendar
+//
+//  Created by Vy Le on 6/10/20.
+//  Copyright © 2020 Vy Le. All rights reserved.
+//
+
+import UIKit
+
+class UserSettingsViewController: UIViewController {
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
