dafcf9ca59e3a46dd720fc4ec2a2d8bd0739fcb6
Vy Le <lethaovy2001@gmail.com>
2020-06-10 06:30:11 -0500

    add more properties for EventViewModel

--- Calendar/DailyTasksScreen/ViewModel/EventViewModel.swift
+++ Calendar/DailyTasksScreen/ViewModel/EventViewModel.swift
@@ -37,4 +37,33 @@ extension EventViewModel {
         guard model.location != "" else { return nil }
         return model.location
     }
+    
+    var notes: String? {
+        return model.notes
+    }
+    
+    var alertTime: String? {
+        guard let alertTime = model.alertTime else { return nil }
+        let difference = Calendar.current.dateComponents([.minute, .hour, .day, .weekOfYear], from: alertTime, to: model.startTime)
+        guard
+            let minute = difference.minute,
+            let hour = difference.hour,
+            let day = difference.day,
+            let week = difference.weekOfYear
+        else { return nil }
+        if week != 0 {
+            guard week > 1 else { return "\(week) weeks before" }
+            return "\(week) week before"
+        } else if day != 0 {
+            guard day > 1 else { return "\(day) days before" }
+            return "\(day) day before"
+        } else if hour != 0 {
+            guard hour > 1 else { return "\(hour) hours before" }
+            return "\(hour) hour before"
+        } else if minute != 0 {
+            guard minute > 1 else { return "\(minute) minutes before" }
+            return "\(minute) minute before"
+        }
+        return nil
+    }
 }
