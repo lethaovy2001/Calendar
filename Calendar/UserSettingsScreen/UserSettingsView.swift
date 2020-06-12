07197f9999e5c0640ab23d7fbaac799f6cd73723
Vy Le <lethaovy2001@gmail.com>
2020-06-12 17:21:15 -0500

    basic layout for UserSettings

---
+++ Calendar/UserSettingsScreen/UserSettingsView.swift
@@ -0,0 +1,51 @@
+//
+//  UserSettingsView.swift
+//  Calendar
+//
+//  Created by Vy Le on 6/12/20.
+//  Copyright © 2020 Vy Le. All rights reserved.
+//
+
+import UIKit
+
+final class UserSettingsView: UIView {
+    // MARK: - Properties
+    private let logoutButton = RoundedButton(title: "LOGOUT", color: AppColor.primaryColor)
+    private let backButton = BackButton()
+    
+    // MARK: - Initializer
+    init() {
+        super.init(frame: .zero)
+        setup()
+        self.backgroundColor = .white
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup
+    private func setup() {
+        addSubviews()
+        setupConstraints()
+    }
+    
+    private func addSubviews() {
+        addSubview(logoutButton)
+        addSubview(backButton)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 36),
+            backButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 36)
+        ])
+        NSLayoutConstraint.activate([
+            logoutButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            logoutButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            logoutButton.widthAnchor.constraint(equalToConstant: 140),
+            logoutButton.heightAnchor.constraint(equalToConstant: 48)
+        ])
+    }
+}
--- Calendar/UserSettingsScreen/UserSettingsViewController.swift
+++ Calendar/UserSettingsScreen/UserSettingsViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class UserSettingsViewController: UIViewController {
+    // MARK: - Initializer
+    private let mainView = UserSettingsView()
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        setup()
+    }
+    
+    // MARK: - Setup
+    private func setup() {
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(mainView)
+        NSLayoutConstraint.activate([
+            mainView.topAnchor.constraint(equalTo: view.topAnchor),
+            mainView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            mainView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            mainView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
