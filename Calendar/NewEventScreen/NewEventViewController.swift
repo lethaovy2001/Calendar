5978a52433673ad77e511b896a10fd3a5c1e7d6b
Vy Le <lethaovy2001@gmail.com>
2020-06-16 03:54:49 -0500

    finish refactor save event

--- Calendar/NewEventScreen/NewEventViewController.swift
+++ Calendar/NewEventScreen/NewEventViewController.swift
@@ -14,7 +14,6 @@ final class NewEventViewController: UIViewController {
     private let mainView = NewEventView()
     private var alertOptions = Constants.setAlertOptions
     weak var keyboardDelegate: KeyboardDelegate?
-    private var selectedComponent: Calendar.Component?
     
     // MARK: - Initializer
     init(database: Database = FirebaseService.shared) {
@@ -76,19 +75,7 @@ final class NewEventViewController: UIViewController {
         guard let event = mainView.getSavedEvent() else {
             return
         }
-        guard
-            let time = mainView.getTimeSetForAlert(),
-            let component = selectedComponent,
-            let alertDate = Calendar.current.date(byAdding: component,
-                                                  value: -time,
-                                                  to: event.startTime)
-        else {
-            database.save(event: event)
-            return
-        }
-        var updateEvent = event
-        updateEvent.alertTime = alertDate
-        database.save(event: updateEvent)
+        database.save(event: event)
     }
 }
 
@@ -110,17 +97,7 @@ extension NewEventViewController: UITableViewDataSource {
 
 extension NewEventViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch alertOptions[indexPath.row] {
-        case .minute:
-            selectedComponent = .minute
-        case .hour:
-            selectedComponent = .hour
-        case .day:
-            selectedComponent = .day
-        case .month:
-            selectedComponent = .month
-        }
-        mainView.updateAlert(option: alertOptions[indexPath.row])
+        mainView.alertOption = alertOptions[indexPath.row]
     }
 }
 
--- Calendar/NewEventScreen/View/NewEventView.swift
+++ Calendar/NewEventScreen/View/NewEventView.swift
@@ -76,6 +76,23 @@ final class NewEventView: UIView {
     private var selectedTimeLabel: CustomLabel?
     var keyboardFrame = CGRect()
     private var doNotRepeatButtonBottomAnchor: NSLayoutConstraint?
+    private var selectedComponent: Calendar.Component?
+    var alertOption: AlertOptions? {
+        didSet {
+            switch alertOption {
+            case .minute:
+                selectedComponent = .minute
+            case .hour:
+                selectedComponent = .hour
+            case .day:
+                selectedComponent = .day
+            case .month:
+                selectedComponent = .month
+            case .none:
+                break
+            }
+        }
+    }
     
     // MARK: - Initializer
     init() {
@@ -279,6 +296,7 @@ extension NewEventView {
         datePickerView.tapDelegate = self
         viewController.keyboardDelegate = self
         noteTextView.delegate = viewController
+        notificationView.doneTapGestureDelegate = self
     }
     
     func addTapGesture(target: UIViewController, selector: Selector) {
@@ -288,9 +306,9 @@ extension NewEventView {
         tapRecognizer.cancelsTouchesInView = false
     }
     
-    func updateAlert(option: AlertOptions) {
+    func updateAlert() {
         let optionString: String?
-        switch option {
+        switch alertOption {
         case .minute:
             optionString = "minute"
         case .hour:
@@ -299,6 +317,8 @@ extension NewEventView {
             optionString = "day"
         case .month:
             optionString = "week"
+        case .none:
+            optionString = ""
         }
         guard
             let component = optionString,
@@ -345,18 +365,22 @@ extension NewEventView {
             startTimeLabel.showWarningAnimation()
             return nil
         }
-        let event = Event(
+        var event = Event(
             name: name,
             startTime: startTime,
             endTime: endTime,
             location: locationTextField.text,
             notes: noteTextView.text
         )
+        guard
+            let time = notificationView.getTimeTextField(),
+            let component = selectedComponent,
+            let alertDate = Calendar.current.date(byAdding: component,
+                                              value: -time,
+                                              to: startTime)
+        else { return event }
+        event.alertTime = alertDate
         return event
-    }
-    
-    func getTimeSetForAlert() -> Int? {
-        return notificationView.getTimeTextField()
     }
 }
 
@@ -397,5 +421,12 @@ extension NewEventView {
     
     func endEditing() {
         noteTextView.endEditing()
+    }
+}
+
+// MARK: - DoneTapGestureDelegate
+extension NewEventView: DoneTapGestureDelegate {
+    func didTap() {
+        updateAlert()
     }
 }
